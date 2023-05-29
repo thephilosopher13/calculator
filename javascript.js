78b538299e976bfd91d25a6bd909e18eeca8f07c
Coloqs <60373194+thephilosopher13@users.noreply.github.com>
2023-05-29 16:54:44 +0800

    Added way to change display number

--- javascript.js
+++ javascript.js
@@ -2,8 +2,29 @@ const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
+const display = document.getElementById('display')
+const numberButtons = document.querySelectorAll('.number')
+let displayNumber = "0";
+function handleClick(event) {
+    const clickedItemId = event.target.id;
+    return clickedItemId
+}
+
+function updateDisplay() {
+    display.textContent = displayNumber
+}
 
 const operate = (a,b,event) => {
     const operator = event.target.id;
     return ([operator](a,b))
 }
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        var buttonNumber = handleClick(event);
+        const getDisplayNumber = displayNumber;
+        ((displayNumber == "0" ? (displayNumber = buttonNumber) : (displayNumber = getDisplayNumber + buttonNumber)))
+        updateDisplay()
+        console.log(displayNumber);
+    });
+});
